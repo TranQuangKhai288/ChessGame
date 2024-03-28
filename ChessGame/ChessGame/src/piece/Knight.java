7ad14dc8f057a3c7f2cb0182d452b02e32a9bf94
trần khải <21522185@gm.uit.edu.vn>
2024-03-28 22:32:04 +0700

    first commit

---
+++ ChessGame/ChessGame/src/piece/Knight.java
@@ -0,0 +1,32 @@
+package piece;
+
+import main.GamePanel;
+
+public class Knight extends Piece {
+
+	public Knight(int color, int col, int row) {
+		super(color, col, row);
+		
+		if(color == GamePanel.WHITE) {
+			image = getImage("/piece/wN");
+		}else {
+			image = getImage("/piece/bN");
+		}
+		
+	}
+	
+public boolean canMove(int targetCol, int targetRow) {
+		
+		if(isWithinBoard(targetCol, targetRow))
+		{
+			if((Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow))==2) {
+				if(isValidSquare(targetCol, targetRow)) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+}
