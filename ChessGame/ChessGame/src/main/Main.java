46eba2ce900d55bf99bbd5bfa1d82afe26afeb7b
sang <sangfc774@gmail.com>
2024-04-29 11:23:57 +0700

    initial layout dashboard

--- ChessGame/ChessGame/src/main/Main.java
+++ ChessGame/ChessGame/src/main/Main.java
@@ -8,17 +8,21 @@ public class Main {
 		// TODO Auto-generated method stub
 		JFrame window = new JFrame("Chess Game");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
+//		window.setResizable(false);
+		
+		//DashBoard
+		DashBoard dashboard = new DashBoard();
+		
 		
 		//add game panel(VIEW) into the window
-		GamePanel gp = new GamePanel();
-		window.add(gp);
+//		GamePanel gp = new GamePanel();
+		window.add(dashboard);
 		window.pack();
 		
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		
-		gp.launchGame();
+//		
+//		gp.launchGame();
 		
 	}
 
