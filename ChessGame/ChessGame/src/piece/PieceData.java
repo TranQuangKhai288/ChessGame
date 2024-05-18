e24f42b46f147aec0bebfecbada943177023bc38
Aransu <thithanhcong102@gmail.com>
2024-05-18 22:13:12 +0700

    done save game

---
+++ ChessGame/ChessGame/src/piece/PieceData.java
@@ -0,0 +1,16 @@
+package piece;
+
+public class PieceData {
+    private int col;
+    private int row;
+    private int color;
+    private String type;
+    public PieceData(int col, int row, int color, String type) {
+        this.col = col;
+        this.row = row;
+        this.color = color;
+        this.type = type;
+    }
+
+    // Getters and setters
+}
