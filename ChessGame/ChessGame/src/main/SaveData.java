6a3c7606466138f0c2cef7ae6c9be135206c2d87
Aransu <thithanhcong102@gmail.com>
2024-06-06 21:45:07 +0700

    done

--- ChessGame/ChessGame/src/main/SaveData.java
+++ ChessGame/ChessGame/src/main/SaveData.java
@@ -5,12 +5,20 @@ import java.util.ArrayList;
 import piece.PieceData;
 
 public class SaveData {
-    String name;
+    String game;
     ArrayList<PieceData> pieces;
     int currentColor;
-    public SaveData(String name, ArrayList<PieceData> pieces, int currentColor) {
-        this.name = name;
+    boolean whiteKingSide;
+    boolean whiteQueenSide;
+    boolean blackKingSide;
+    boolean blackQueenSide;
+    public SaveData(String game, ArrayList<PieceData> pieces, int currentColor, boolean whiteKingSide, boolean whiteQueenSide, boolean blackKingSide, boolean blackQueenSide) {
+        this.game = game;
         this.pieces = pieces;
         this.currentColor = currentColor;
+        this.blackKingSide = blackKingSide;
+        this.blackQueenSide = blackQueenSide;
+        this.whiteKingSide = whiteQueenSide;
+        this.whiteKingSide = whiteKingSide;
     }
 }
